e86d83eaf65f3116d0d8d0d7ceb120028508d68b
harshdeepgill <er.harshdeepgill@gmail.com>
2023-09-19 22:45:50 +0530

    done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,7 @@ import "./Navbar.scss"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const Navbar = ({slider,currTab, handleNavlinkScroll}) => {
-
-  
-
-  
+const Navbar = ({currTab, handleNavlinkScroll}) => {
 
   return (
     <div className='main'>
